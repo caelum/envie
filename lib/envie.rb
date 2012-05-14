cc828896c5b7b64d13cdc2492416f42fe2578deb
Ram Dobson <ram.dobson@solsystemscompany.com>
2012-05-14 18:24:02 -0400

    avoid polluting the global namespace with feature

--- lib/envie.rb
+++ lib/envie.rb
@@ -2,34 +2,35 @@ require 'envie/env'
 require 'envie/featurable'
 module Envie
   class << self
-    
+
     attr_reader :current
-    
+
     def all
       @envs ||= {}
     end
-    
+
     def method_missing(name)
-      at(name)
+      if name =~ /^(.*)\?$/
+        feature = $1.to_sym
+        has?(feature)
+      else
+        at(name)
+      end
     end
-    
+
     def at(name)
       all[name] ||= Env.new
     end
-    
+
     def has?(name)
       @current.has?(name)
     end
-    
+
     def use(name)
       @current = at(name)
     end
 
   end
-  
-  Envie.use :production
-end
 
-class Object
-  include Envie::Featurable
+  Envie.use :production
 end
--- spec/unit/envie_spec.rb
+++ spec/unit/envie_spec.rb
@@ -1,23 +1,32 @@
 require 'spec_helper'
 
 describe Envie do
-  
+
   context "using envie" do
-    
+
     it "should not duplicate environments at each call" do
       Envie.extra.should == Envie.extra
     end
-    
+
     it "using should change current the one" do
       Envie.use(:extra)
       Envie.current.should == Envie.extra
     end
-    
+
     it "should check with the current scope" do
       Envie.current.should_receive(:has?).with(:magic).and_return(true)
       Envie.has?(:magic).should == true
     end
-    
+
+    it "should check with the current scope" do
+      Envie.current.should_receive(:has?).with(:magic).and_return(true)
+      Envie.has?(:magic).should == true
+    end
+
+    it "should respond to foo? like has?(:foo)" do
+      Envie.current.should_receive(:has?).with(:magic).and_return(true)
+      Envie.magic?.should == true
+    end
   end
-  
+
 end
